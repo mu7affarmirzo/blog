7b5cfc3e2a9615188b9aa05e06e05c2483259ab3
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2020-05-06 01:09:36 +0500

    similar posts

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail
 from .forms import EmailPostForm,CommentForm
 from .models import PostModel,CommentModel
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -53,10 +54,15 @@ def post_detail(request, year, month, day, post):
 	else:
 		comment_form = CommentForm()
 
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = PostModel.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
 	return render(request,'blog/post/detail.html',{'post':post,
 													'comments': comments,
 													'new_comment': new_comment,
-													'comment_form': comment_form})
+													'comment_form': comment_form,
+													'similar_posts': similar_posts})
 
 def post_share(request,post_id):
 	post = get_object_or_404(PostModel,id=post_id,status='published')
